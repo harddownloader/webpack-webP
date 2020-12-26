975a1861b669cb3c7709e3b57ae9095664cefebd
harddownloader <super-olegpa@ukr.net>
2020-12-27 01:20:00 +0200

    convert imgs to webp from forlder 'img' to 'compressed_images'

---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,48 @@
+const Webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+
+// code for webp convertation
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
+
+imagemin(['img/*'], {
+  destination: 'compressed_images',
+  plugins: [imageminWebp({quality: 50})]
+}).then(() => {
+  console.log('Done!');
+});
+
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  stats: 'errors-only',
+  bail: true,
+  output: {
+    filename: 'js/[name].[chunkhash:8].js',
+    chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s?css/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
